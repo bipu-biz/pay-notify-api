3d1a7a3917fa929ab1775c5a199920f0970da0fb
bipu-biz <bipulbharti2006@gmail.com>
2026-04-18 19:46:30 +0530

    fix: add tls support for redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,8 @@
 import Redis from "ioredis";
 
 const redis = new Redis(process.env.REDIS_URL,{
-    maxRetriesPerRequest: null
+    maxRetriesPerRequest: null,
+    tls: process.env.NODE_ENV === 'production' ? {} : undefined
 })
 
 redis.on('connect',()=>{
